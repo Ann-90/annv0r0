e8a63320143846db7a99d7654632db806ae68474
annv0r0 <annvv0r0@gmail.com>
2023-08-08 15:33:36 +0600

    translation

--- translations.js
+++ translations.js
@@ -1,66 +1,66 @@
 let translations = {
-	ru: {
-		Home: "Главная",
-		About: "О себе",
-		Skills: "Навыки",
-		Portfolio: "Портфолио",
-		Contacts: "Контакты",
-		my__name: "Анна <br/> Воробьева",
-		post: "JUNIOR ФРОНТЕНД РАЗРАБОТЧИК<br />Москва",
-		about_1:
-			"Здравствуйте. Меня зовут Анна. Я - джуниор веб-разработчик. Веб-технологии изучаю самостоятельно с сентября 2020 года.",
-		about_2:
-			"С января по июль 2021 года я работала помощником веб-разработчика, занимаясь отрисовкой интерфейсов по готовым шаблонам Figma при помощи React и TypeScript. В преиод обучения знакомилась с широким стеком тенологий: немного познакомилась с базовым фнкуционалом Redux, NodeJS, ModgoDB.",
-		about_3:
-			"С июля 2021 года знакомлюсь с языком PHP: в круг моих обязанностей входит верстка, работа с реляционной базой данных, использование сторонних API (например, Yandex maps).",
-		about_4:
-			'Также в июле проходила дополнительное обучение в ТГУ по программе "Data аналитик": знакомилась с основами PostgreSQL, Python и библиотеками машинного обучения.',
+  ru: {
+    Home: 'Главная',
+    About: 'О себе',
+    Skills: 'Навыки',
+    Portfolio: 'Портфолио',
+    Contacts: 'Контакты',
+    my__name: 'Анна <br/> Воробьева',
+    post: 'ФРОНТЕНД РАЗРАБОТЧИК<br />Москва',
+    about_1:
+      'Здравствуйте. Меня зовут Анна. Я - джуниор веб-разработчик. Веб-технологии изучаю самостоятельно с сентября 2020 года.',
+    about_2:
+      'С января по июль 2021 года я работала помощником веб-разработчика, занимаясь отрисовкой интерфейсов по готовым шаблонам Figma при помощи React и TypeScript. В преиод обучения знакомилась с широким стеком тенологий: немного познакомилась с базовым фнкуционалом Redux, NodeJS, ModgoDB.',
+    about_3:
+      'С июля 2021 года знакомлюсь с языком PHP: в круг моих обязанностей входит верстка, работа с реляционной базой данных, использование сторонних API (например, Yandex maps).',
+    about_4:
+      'Также в июле проходила дополнительное обучение в ТГУ по программе "Data аналитик": знакомилась с основами PostgreSQL, Python и библиотеками машинного обучения.',
 
-		about_5: 'Работаю с git: ссылку на мой аккаунт можно увидеть в разделе "Портфолио"',
+    about_5: 'Работаю с git: ссылку на мой аккаунт можно увидеть в разделе "Портфолио"',
 
-		about_6:
-			"Я - специалист с нестандартным бэкграундом. До недавнего времени работала в глубоко гуманитарной сфере. Так как учусь самостоятельно, не боюсь разбинаться с темами, с котрыми не работала.",
-		about_7:
-			"В данный момент хочу больше поработать с React и NodeJS, но всегда готова к новым открытиям. Хочу развиваться вперед и вглубь, погружаясь в тонкости разработки и выполняя полезную работу в интересной команде.",
-		Skills: "Что я умею",
-		skills__title: "Я работаю с такими технологиями:",
-		Portfolio: "Портфолио",
-		Contacts: "Контакты",
-		contacts__text: "Хотите уточнить информацию? <br/> Напишите мне",
-		contacts__send: "Отправить сообщение",
-		contacts__appeal: "Свяжитесь со мной через <br /> Telegram / WhatsApp / Skype",
-		name: "",
-		address: "",
-		letter: "",
-	},
+    about_6:
+      'Я - специалист с нестандартным бэкграундом. До недавнего времени работала в глубоко гуманитарной сфере. Так как учусь самостоятельно, не боюсь разбинаться с темами, с котрыми не работала.',
+    about_7:
+      'В данный момент хочу больше поработать с React и NodeJS, но всегда готова к новым открытиям. Хочу развиваться вперед и вглубь, погружаясь в тонкости разработки и выполняя полезную работу в интересной команде.',
+    Skills: 'Что я умею',
+    skills__title: 'Я работаю с такими технологиями:',
+    Portfolio: 'Портфолио',
+    Contacts: 'Контакты',
+    contacts__text: 'Хотите уточнить информацию? <br/> Напишите мне',
+    contacts__send: 'Отправить сообщение',
+    contacts__appeal: 'Свяжитесь со мной через <br /> Telegram / WhatsApp / Skype',
+    name: '',
+    address: '',
+    letter: '',
+  },
 
-	en: {
-		Home: "Home",
-		About: "About",
-		Skills: "Skills",
-		Portfolio: "Portfolio",
-		Contacts: "Contacts",
-		my__name: "Ann <br/> Vorobyeva",
-		post: "JUNIOR FRONT-END DEVELOPER<br />Moscow",
-		about_1: "Hello! My name is Ann. I am a junior frontend-developer.",
-		about_2:
-			" Currently I am working in a company rendering web-pages with Figma templates as examples. I am making interactive elements with Next.js (React framework). So I got used to working using frameforks which make development faster and more convinient.",
-		about_3:
-			"If you are interested in people with unconventional experience I submit my candidacy. Until recently I have been working in a humanitarian field. I study computer science on my own. I deal with new information well.",
-		about_4: "I want to move forward doing useful job in a good team",
-		about_5: "",
-		about_6: "",
-		about_7: "",
-		Skills: "Skills",
-		skills__title: "I work in such programms as:",
-		Portfolio: "Portfolio",
-		Contacts: "Contacts",
-		contacts__text: "Want to know more or just chat? <br />You are welcome!",
-		contacts__send: "Send message",
-		contacts__appeal: "Contact me on <br /> Telegram / WhatsApp / Skype",
-		name: "",
-		address: "",
-		letter: "",
-	},
+  en: {
+    Home: 'Home',
+    About: 'About',
+    Skills: 'Skills',
+    Portfolio: 'Portfolio',
+    Contacts: 'Contacts',
+    my__name: 'Ann <br/> Vorobyeva',
+    post: 'FRONT-END DEVELOPER<br />Moscow',
+    about_1: 'Hello! My name is Ann. I am a junior frontend-developer.',
+    about_2:
+      ' Currently I am working in a company rendering web-pages with Figma templates as examples. I am making interactive elements with Next.js (React framework). So I got used to working using frameforks which make development faster and more convinient.',
+    about_3:
+      'If you are interested in people with unconventional experience I submit my candidacy. Until recently I have been working in a humanitarian field. I study computer science on my own. I deal with new information well.',
+    about_4: 'I want to move forward doing useful job in a good team',
+    about_5: '',
+    about_6: '',
+    about_7: '',
+    Skills: 'Skills',
+    skills__title: 'I work in such programms as:',
+    Portfolio: 'Portfolio',
+    Contacts: 'Contacts',
+    contacts__text: 'Want to know more or just chat? <br />You are welcome!',
+    contacts__send: 'Send message',
+    contacts__appeal: 'Contact me on <br /> Telegram / WhatsApp / Skype',
+    name: '',
+    address: '',
+    letter: '',
+  },
 };
 export default translations;
