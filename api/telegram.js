1de92243127808842206e9773aaf586ebff50860
annv0r0 <annvv0r0@gmail.com>
2023-08-08 15:19:18 +0600

    vercel cli

--- api/telegram.js
+++ api/telegram.js
@@ -1,11 +1,12 @@
-let CHAT_ID = "677082724";
+let CHAT_ID = '677082724';
 /* if (process.env.NODE_ENV === 'development') {
 	CHAT_ID = '@test_gm'
 }  */
 
-export default async (message) => {
+/* export default async (message) => {
 	const url = `https://api.telegram.org/bot1789943012:AAEEHYz9IbmICjQ4zweitB05dXj5eV39TtM/sendMessage?
         text=${encodeURIComponent(message)}&chat_id=${CHAT_ID}&parse_mode=html`;
 	const { ok } = await (await fetch(url)).json();
 	return ok;
 };
+ */
--- api/telegramHandle.js
+++ api/telegramHandle.js
@@ -1,25 +1,25 @@
-import telegram from "../../api/telegram.js";
+// import telegram from "../../api/telegram.js";
 
-export default async (req, res) => {
-	const { name, email, letter } = req.body;
-	// console.log("I am here");
-	const message = `<b>You have new message:</b>
+// export default async (req, res) => {
+// 	const { name, email, letter } = req.body;
+// 	// console.log("I am here");
+// 	const message = `<b>You have new message:</b>
 
-			name: ${name}
-			email: ${email}
-			letter: ${letter}
+// 			name: ${name}
+// 			email: ${email}
+// 			letter: ${letter}
 
-		`;
+// 		`;
 
-	// console.log(message);
-	try {
-		// await telegram(message);
-		res.statusCode = 200;
-		res.setHeader("Content-Type", "application/json");
-		res.end(JSON.stringify({ ok: true }));
-	} catch {
-		res.statusCode = 500;
-		res.setHeader("Content-Type", "application/json");
-		res.end(JSON.stringify({ ok: false, url: "www.no.sad" }));
-	}
-};
+// 	// console.log(message);
+// 	try {
+// 		// await telegram(message);
+// 		res.statusCode = 200;
+// 		res.setHeader("Content-Type", "application/json");
+// 		res.end(JSON.stringify({ ok: true }));
+// 	} catch {
+// 		res.statusCode = 500;
+// 		res.setHeader("Content-Type", "application/json");
+// 		res.end(JSON.stringify({ ok: false, url: "www.no.sad" }));
+// 	}
+// };
